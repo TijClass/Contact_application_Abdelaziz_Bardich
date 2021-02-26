bbf0f0d46bf798832aa59df3552bdb7fc064954a
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2021-02-26 13:51:51 +0100

    adding jquery js

--- index.php
+++ index.php
@@ -237,6 +237,7 @@
         </div>
       </div>
     <script src="./assets/vendor/bootstrap/bootstrap.min.js"></script>
+    <script src="./assets/vendor/jquery/jquery.min.js"></script>
     <script src="./assets/js/script.js"></script>
 </body>
 </html>
